da299d6eb1184fdd306be9b0b2ecd1e5e9679fb2
jensen1206 <jens@harz-web.com>
2022-04-13 09:36:14 +0200

    first commit

---
+++ includes/class-wp-facebook-importer-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://wwdh.de
+ * @since      1.0.0
+ *
+ * @package    Wp_Facebook_Importer
+ * @subpackage Wp_Facebook_Importer/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Facebook_Importer
+ * @subpackage Wp_Facebook_Importer/includes
+ * @author     Jens Wiecker <email@jenswiecker.de>
+ */
+class Wp_Facebook_Importer_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wp-facebook-importer',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
